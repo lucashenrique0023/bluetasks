26c27d1fd287142f7b0d8ff6ec319f106705a6ed
detectiverust93 <lucas.henrique0023@gmail.com>
2020-09-25 23:24:44 -0300

    Exibindo tarefas cadastradas na tabela
    Novidades:
    - componentDidMount()
    - Logica de renderizacao com item vazio depois preenchendo
     para renderizar novamente.
    
    Configuramos uma classe para retornar objetos que serao preenchidos na tabela.
    
    Criamos um componente da tabela e criamos seus filhos dentro do priprio
    componente de tabela visto que estao intimamente atrelados.
    
    utilizamos o ciclo de vida do componente para popular os dados da tabela
    e renderizar novamente.

--- src/api/TaskService.js
+++ src/api/TaskService.js
@@ -1,9 +1,9 @@
 class TaskService {
     constructor() {
         this.tasks = [
-            { id: 1, description: "Tarefa 2", WhenToDo: "01/01/2030", done: false },
-            { id: 1, description: "Tarefa 2", WhenToDo: "01/01/2030", done: false },
-            { id: 1, description: "Tarefa 2", WhenToDo: "01/01/2030", done: false }
+            { id: 1, description: "Tarefa 1", WhenToDo: "01/01/2030", done: false },
+            { id: 2, description: "Tarefa 2", WhenToDo: "02/01/2030", done: true },
+            { id: 3, description: "Tarefa 3", WhenToDo: "03/01/2030", done: false }
         ]
     }
 
--- src/components/TaskListTable.js
+++ src/components/TaskListTable.js
@@ -1,40 +1,61 @@
 import React, { Component } from 'react';
+import TaskService from '../api/TaskService';
 
 class TaskListTable extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            tasks: []
+        }
+    }
+
     render() {
         return (
-            <table>
-                <TableHeader />
-                <TableBody />
-            </table>
+                <table className="table table-striped">
+                    <TableHeader />
+                    <TableBody tasks={this.state.tasks}/>
+                </table>
         );
+    }
+
+    componentDidMount() {
+        this.listTasks();
+    }
+ 
+    listTasks(){
+        this.setState({ tasks: TaskService.list() })
     }
 }
 
 const TableHeader = () => {
     return (
-        <thead>
+        <thead className="thead-dark">
             <tr>
-                <td>Status</td>
-                <td>Descricao</td>
-                <td>Data</td>
-                <td>Acoes</td>
+                <th scope="col">Status</th>
+                <th scope="col">Descricao</th>
+                <th scope="col">Data</th>
+                <th scope="col">Acoes</th>
             </tr>
         </thead>
     )
 }
 
-const TableBody = () => {
+const TableBody = (props) => {
     return (
         <tbody>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+        {props.tasks.map(task => 
+            <tr key={task.id}>
+                <td><input type="checkbox" checked={task.done}/></td>
+                <td>{task.description}</td>
+                <td>{task.WhenToDo}</td>
+                <td>
+                    <input type="button" className="btn btn-primary" value="Editar"/>
+                    &nbsp;<input type="button" className="btn btn-danger" value="Excluir"/>
+                </td>
             </tr>
+        )}
         </tbody>
-
     )
 }
 
