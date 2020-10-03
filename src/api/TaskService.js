ad8fc2eb0e50415490637e4dd5cc26fb34ebfea9
detectiverust93 <lucas.henrique0023@gmail.com>
2020-10-03 15:23:15 -0300

    Excluindo as tarefas no back-end

--- src/api/TaskService.js
+++ src/api/TaskService.js
@@ -22,8 +22,10 @@ class TaskService {
         return this.tasks.filter(t => t.id === id)[0];
     }
 
-    delete(id){
-        this.tasks = this.tasks.filter(task => task.id !== id)
+    delete(id, onDelete, onError){
+        Axios.delete(`${API_ENDPOINT}/tasks/${id}`, this.buildAuthHeader())
+            .then(() => onDelete())
+            .catch(e => onError(e));
     }
 
     save(task){
--- src/components/TaskListTable.js
+++ src/components/TaskListTable.js
@@ -80,9 +80,14 @@ class TaskListTable extends Component {
 
     onDeleteHandler(id){
         if (window.confirm("Deseja mesmo remover essa tarefa?")){
-            TaskService.delete(id);
-            this.listTasks();
-            toast.success("Tarefa removida!", { position: toast.POSITION.BOTTOM_LEFT })
+            TaskService.delete(id, 
+                () => {
+                    this.listTasks();
+                    toast.success("Tarefa removida!", { position: toast.POSITION.BOTTOM_LEFT })
+                },
+                error => this.setErrorState(error)
+            );
+            
         }
         
     }
