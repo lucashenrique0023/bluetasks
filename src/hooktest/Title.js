4b3ee75dd2b3db7750f71c5e5e3c249f7de8d41e
detectiverust93 <lucas.henrique0023@gmail.com>
2020-10-05 20:34:05 -0300

    Introducao ao Hooks

---
+++ src/hooktest/Title.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react'
+
+const Title = () => {
+
+    // Hooks
+    const [ title, setTitle ] = useState(null);
+
+    useEffect(() => {
+        document.title = title;
+    }, [ title ])
+
+
+    return (
+        <div className="App">
+            <form>
+                <input type="text" value={title} onChange={(e) => setTitle(e.target.value)}/>
+            </form>
+        </div>
+    ); 
+}
+
+export default Title;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Title from './hooktest/Title';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Title />
   </React.StrictMode>,
   document.getElementById('root')
 );
