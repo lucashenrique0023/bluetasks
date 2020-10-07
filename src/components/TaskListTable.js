b405b1654e0393845eb3a2f0b33c2e16cc9167af
detectiverust93 <lucas.henrique0023@gmail.com>
2020-10-07 20:07:44 -0300

    Listagem de tarefas usando hooks

--- src/components/TaskListTable.js
+++ src/components/TaskListTable.js
@@ -1,84 +1,25 @@
-import React, { Component } from 'react';
-import { toast, ToastContainer } from 'react-toastify';
-import TaskService from '../api/TaskService';
+import React, { useContext, useEffect } from 'react';
+import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
 import { Redirect } from 'react-router-dom';
 import Alert from './Alert';
-import AuthService from '../api/AuthService';
 import Spinner from './Spinner';
 import Moment from 'react-moment';
+import { useTasks } from '../hooks/useTasks';
+import { AuthContext } from '../hooks/useAuth';
 
-class TaskListTable extends Component {
-    constructor(props){
-        super(props);
+const TaskListTable = () => {
+    const auth = useContext(AuthContext);
+    const tasks = useTasks();
 
-        this.state = {
-            tasks: [],
-            editId: 0,
-            loading: false,
-            alert: null
-        }
+    useEffect(() => {
+        if (auth.credentials.username !== null){
+            tasks.list();
+        } 
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [ auth.credentials ]);
 
-        this.onDeleteHandler = this.onDeleteHandler.bind(this);
-        this.onStatusChangeHandler = this.onStatusChangeHandler.bind(this);
-        this.onEditHandler = this.onEditHandler.bind(this);
-    }
-
-    render() {
-        if (!AuthService.isAuthenticated()){
-            return <Redirect to="/login" />
-        }
-
-        if (this.state.editId !== 0){
-            return <Redirect to={`form/${this.state.editId}`} />
-        }
-
-        return (
-                <>
-                    <h1>Lista de Tarefas</h1>
-                    { this.state.alert != null ? <Alert message={this.state.alert} /> : "" }
-                    { this.state.loading ?  <Spinner /> : 
-                        <table className="table table-striped">
-                            <TableHeader />
-                            {this.state.tasks.length > 0 ?
-                                <TableBody tasks={this.state.tasks} 
-                                            onDelete={this.onDeleteHandler}
-                                            onEdit={this.onEditHandler}
-                                            onStatusChange={this.onStatusChangeHandler}
-                                            />
-                                :
-                                <EmptyTableBody />
-                            }
-                            
-                        </table>
-                    }
-                    <ToastContainer autoClose={1500} />
-                    
-                </>
-        );
-    }
-
-    componentDidMount() {
-        this.listTasks();
-    }
- 
-    listTasks(){
-        if (!AuthService.isAuthenticated()){
-            return;
-        }
-
-        this.setState({ loading: true });
-        TaskService.list(
-            tasks => this.setState({ tasks: tasks, loading: false }),
-            error => this.setErrorState(error)
-        )
-    }
-
-    setErrorState(error){
-        this.setState({ alert: `Erro na requisicao: ${error.message}`, loading: false });
-    }
-
-    onDeleteHandler(id){
+    /*onDeleteHandler(id){
         if (window.confirm("Deseja mesmo remover essa tarefa?")){
             TaskService.delete(id, 
                 () => {
@@ -90,80 +31,65 @@ class TaskListTable extends Component {
             
         }
         
+    }*/
+
+    if (!auth.isAuthenticated()){
+        return <Redirect to="/login" />
     }
 
-    onEditHandler(id){
-        this.setState({editId: id})
-    }
-
-    onStatusChangeHandler(task){
-        task.done = !task.done;
-    
-        TaskService.save(task,
-            () => {
-                const tasks = this.state.tasks.map(t => t.id !== task.id ? t : task);
-                this.setState({ tasks: tasks });
-            },
-            (error => this.setErrorState(error)));
-    }
-
-}
-
-const TableHeader = () => {
     return (
-        <thead className="thead-dark">
-            <tr>
-                <th scope="col">Status</th>
-                <th scope="col">Descricao</th>
-                <th scope="col">Data</th>
-                <th scope="col">Acoes</th>
-            </tr>
-        </thead>
-    )
-}
-
-const TableBody = (props) => {
-    return (
-        <tbody>
-        {props.tasks.map(task => 
-            <tr key={task.id}>
-                <td>
-                    <input type="checkbox"
-                        checked={task.done}
-                        onChange={(event) => props.onStatusChange(task)}
-                        />
-                </td>
-                <td>{task.done ? <s>{task.description}</s> : task.description}</td>
-                <td>{task.done ? 
-                                <s><Moment format="DD/MM/YYYY">{task.whenToDo}</Moment></s> 
-                                : <Moment format="DD/MM/YYYY">{task.whenToDo}</Moment>}
-                </td>
-                <td>
-                    <input type="button" 
-                            className="btn btn-primary"
-                            value="Editar"
-                            onClick={() => props.onEdit(task.id)}
-                            />
-                    &nbsp;
-                    <input type="button" 
-                            className="btn btn-danger" 
-                            value="Excluir"
-                            onClick={() => props.onDelete(task.id)}
-                            />
-                </td>
-            </tr>
-        )}
-        </tbody>
-    )
-}
-
-const EmptyTableBody = (props) => {
-    return (
-        <tbody>
-            <tr><td colSpan="4">Sem tarefas cadastradas no momento!</td></tr>
-        </tbody>
-
-    )
+        <>
+            <h1>Lista de Tarefas</h1>
+            { tasks.error && <Alert message={tasks.error} /> }
+            { tasks.processing ?  <Spinner /> : 
+                <table className="table table-striped">
+                    <thead className="thead-dark">
+                        <tr>
+                            <th scope="col">Status</th>
+                            <th scope="col">Descricao</th>
+                            <th scope="col">Data</th>
+                            <th scope="col">Acoes</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    { tasks.taskList.length === 0 ? <tr><td colSpan="4">Sem tarefas cadastradas no momento!</td></tr> :
+                        (
+                            tasks.taskList.map(task => 
+                                <tr key={task.id}>
+                                    <td>
+                                        <input type="checkbox"
+                                            checked={task.done}
+                                            onChange={(event) => false}
+                                            />
+                                    </td>
+                                    <td>{task.done ? <s>{task.description}</s> : task.description}</td>
+                                    <td>{task.done ? 
+                                                    <s><Moment format="DD/MM/YYYY">{task.whenToDo}</Moment></s> 
+                                                    : <Moment format="DD/MM/YYYY">{task.whenToDo}</Moment>}
+                                    </td>
+                                    <td>
+                                        <input type="button" 
+                                                className="btn btn-primary"
+                                                value="Editar"
+                                                onClick={() => false}
+                                                />
+                                        &nbsp;
+                                        <input type="button" 
+                                                className="btn btn-danger" 
+                                                value="Excluir"
+                                                onClick={() => false}
+                                        />
+                                    </td>
+                                </tr>
+                            )
+                        )
+                    }
+                    </tbody>     
+                </table>
+            }
+            <ToastContainer autoClose={1500} />   
+        </>
+    );
 }
 
 export default TaskListTable;
