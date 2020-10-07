d0130ff07d4ef2f9b5526a3cfccd38e13b92e95f
detectiverust93 <lucas.henrique0023@gmail.com>
2020-10-07 00:58:52 -0300

    Criando Hook useAuth para autenticacao.

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,58 @@
+const { default: Axios } = require("axios");
+const { useState, useEffect } = require("react");
+const { AUTH_ENDPOINT, CREDENTIALS_NAME } = require("../constants");
+
+export const useAuth = () => {
+    const [ credentials, setCredentials ] = useState({ username: null, displayName: null, token: null});
+    const [ error, setError ] = useState(null);
+    const [ processing, setProcessing] = useState(false);
+
+    // Executa primeira vez que o componente e renderizado.
+    useEffect(() => {
+        loadCredentials();
+    }, []);
+
+    const login = async (username, password) => {
+        
+        const loginInfo = { username: username, password: password }
+        setProcessing(true);
+       
+        try {
+            const response = await Axios.post(`${AUTH_ENDPOINT}/login`, loginInfo)
+            const token = response.headers['authorization'].replace("Bearer ", "");
+            storeCredentials(token);
+            setProcessing(false);
+
+        } catch (error) {
+            console.log(error);
+            setError("O login nao pode ser realizado");
+            setProcessing(false);
+        }
+
+    }
+
+    const logout = () => {
+        sessionStorage.removeItem(CREDENTIALS_NAME);
+        setCredentials({username: null, displayName: null, token: null});
+    }
+
+    const storeCredentials = (token) => {
+        const tokenData = atob(token.split(".")[1]);
+        const credentials = { username: tokenData.sub, displayName: tokenData.displayName, token: token }
+        sessionStorage.setItem(CREDENTIALS_NAME, JSON.stringify(credentials));
+        setCredentials(credentials);
+    }
+
+    const loadCredentials = () => {
+        const storedCredentials = sessionStorage.getItem(CREDENTIALS_NAME);
+        if (storedCredentials !== null){
+            setCredentials(JSON.parse(storedCredentials));
+        }
+    }
+
+    const isAuthenticated = () => {
+        return sessionStorage.getItem(CREDENTIALS_NAME) !== null;
+    }
+
+    return { login, logout, isAuthenticated, credentials, error, processing }
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import CounterComposed from './hooktest/CounterComposed';
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+import React from 'react'
+import ReactDOM from 'react-dom'
+import CounterComposed from './hooktest/CounterComposed'
+import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <CounterComposed />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <CounterComposed />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
